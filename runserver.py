a73798f1491f62067ddfca0bc515a98185ec9e73
runjam <zhangrun@yuhu.tech>
2020-05-22 17:23:19 +0800

    refactor:new branch HUDEX-MM-PRICE-FSC/KRWT

--- runserver.py
+++ runserver.py
@@ -51,7 +51,7 @@ def generate_next_data():
 
 
 
-current_price = 500
+current_price = 600
 
 file_cache={}
 
@@ -93,7 +93,7 @@ if __name__ == '__main__':
     t2.daemon = True
     t2.start()
 
-    app.run(host='0.0.0.0',port=6005)
+    app.run(host='0.0.0.0',port=6006)
 
 
 
