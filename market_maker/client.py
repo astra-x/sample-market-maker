75c257c48c08a6b88d0e262cab582cce94853e9e
runjam <zhangrun@yuhu.tech>
2020-05-27 18:31:17 +0800

    refactor:update branch

--- market_maker/client.py
+++ market_maker/client.py
@@ -283,7 +283,11 @@ class Client(object):
 
         # Reset retry counter on success
         self.retries = 0
-        print("url:{},------->response:{},---->postdict:{}".format(url, response.json(),postdict))
         if response:
-            response=response.json()
+            try:
+                response=response.json()
+            except  Exception as e:
+                print("response--error------>",e)
+                response=""
+        print("url------>:{},response-------->:{},postdict----->:{}".format(url,response,postdict))
         return response
