0cf29b9fcae06a48644d660344a90684fe91b4b4
runjam <zhangrun@yuhu.tech>
2020-09-01 14:25:57 +0800

    feat: reset config

--- market_maker/market_maker_inner.py
+++ market_maker/market_maker_inner.py
@@ -206,12 +206,16 @@ class OrderManager:
             self.get_ticker()
             buy_order=self.prepare_order(-i)
             sell_order=self.prepare_order(i)
-            if i==0:
-                quantity = round(random.uniform(settings.ORDER_START_MIN_SIZE, settings.ORDER_START_MAX_SIZE), 2)
-                buy_order["amount"]=str(quantity)
-                sell_order["amount"]=str(quantity)
-            buy_orders.append(buy_order)
-            sell_orders.append(sell_order)
+            if i == 0:
+                quantity = round(random.uniform(settings.ORDER_START_MIN_SIZE,
+                                                settings.ORDER_START_MAX_SIZE) / settings.VOLUME_MINIFICATION, 2)
+                buy_order["amount"] = str(quantity)
+                sell_order["amount"] = str(quantity)
+                buy_orders.append(buy_order)
+                sell_orders.append(sell_order)
+            else:
+                buy_orders.append(buy_order)
+                sell_orders.append(sell_order)
         orders_created=self.converge_orders(buy_orders, sell_orders)
         time.sleep(self.CycleTime)
         random.shuffle(orders_created)
