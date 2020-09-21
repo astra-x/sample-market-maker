519c0b296fd5a7b5f63fba1bdb5f4dabcadbb0c3
runjam <zhangrun@yuhu.tech>
2020-09-21 17:15:24 +0800

    feat: imple LINK/USDT

--- test/test_gateio.py
+++ test/test_gateio.py
@@ -17,14 +17,14 @@ ws = create_connection("wss://ws.gate.io/v3/",http_proxy_host="127.0.0.1",http_p
 
 
 
-ws.send('{"id":12312, "method":"ticker.subscribe", "params":["BTC_USDT"]}')
+# ws.send('{"id":12312, "method":"ticker.subscribe", "params":["BTC_USDT"]}')
 # ws.send('{"id":12312, "method":"ticker.subscribe", "params":["EOS_USDT"]}')
 # ws.send('{"id":12312, "method":"ticker.subscribe", "params":["ETH_USDT"]}')
 
 # ws.send('{"id":12312, "method":"ticker.subscribe", "params":["BCH_USDT"]}')
 # ws.send('{"id":12312, "method":"ticker.subscribe", "params":["BCHSV_USDT"]}')
 # ws.send('{"id":12312, "method":"ticker.subscribe", "params":["ETC_USDT"]}')
-# ws.send('{"id":12312, "method":"ticker.subscribe", "params":["LINK_USDT"]}')
+ws.send('{"id":12312, "method":"ticker.subscribe", "params":["LINK_USDT"]}')
 # ws.send('{"id":12312, "method":"ticker.subscribe", "params":["YFI_USDT"]}')
 # ws.send('{"id":12312, "method":"ticker.subscribe", "params":["YFII_USDT"]}')
 # ws.send('{"id":12312, "method":"ticker.subscribe", "params":["YFV_USDT"]}')
